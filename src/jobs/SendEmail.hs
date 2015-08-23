466540cb13768b0b89e141a12a1d129e8fa75828
James Vanneman <jvannem@gmail.com>
2015-08-23 16:10:42 -0400

    Add email job

--- src/jobs/SendEmail.hs
+++ src/jobs/SendEmail.hs
@@ -1,4 +1,36 @@
-import Network.Sendgrid.Api(EmailMessage,) 
+import Database.Persist(Entity(..))
+import Network.Sendgrid.Api(EmailMessage) 
+import Schema
 
-main :: IO
+import Snap.Snaplet.Auth(AuthUser)
+import Snap.Snaplet.Auth.Backends.Persistent(SnapAuthUser)
+
+main :: IO ()
 main = do 
+  groups <- allGroups
+  mapM_ sendEmail groups
+
+
+
+allGroups :: IO [Entity Group]
+allGroups = undefined
+
+oneWeek = 7
+
+linksForGroup :: Int -> Entity Group -> IO [Entity Link]
+linksForGroup = undefined
+
+generateEmailFor :: [Entity Link] -> Entity SnapAuthUser -> EmailMessage
+generateEmailFor = undefined
+
+postToSendgrid :: EmailMessage -> IO ()
+postToSendgrid = undefined
+
+usersForGroup :: Entity Group -> IO [Entity SnapAuthUser]
+usersForGroup = undefined
+
+sendEmail :: Entity Group -> IO ()
+sendEmail group = do 
+  recentlyAddedLinks <- linksForGroup oneWeek group 
+  usersInGroup <- usersForGroup group
+  mapM_ (postToSendgrid . generateEmailFor recentlyAddedLinks) usersInGroup 
